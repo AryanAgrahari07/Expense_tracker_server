a073c6e4b728083eaae842ada951218ae75dee13
Aryan Agrahari <94286824+AryanAgrahari07@users.noreply.github.com>
2023-12-28 14:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ mongoose.connect("mongodb://localhost:27017/expense_tracker" , {
 }).catch((e) => {console.log(e)});
 
 
-// app.get("/" , (req, res) =>{
-//     res.send("Nice working");
-// })
-// app.get('*' , (req, res)=>{
-    // res.sendFile(path.resolve(__dirname, 'client/src/pages/home.jsx'))
-// })
-app.listen(3000 , ()=>{
-    console.log("Server is Conneted");
-})
+const port =process.env.PORT || 5000
+
+if(process.env.NODE_ENV === 'production')
+{
+     app.use('/' , express.static('client/build'))
+
+     app.get('*' , (req, res)=>{
+         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+     })
+}
+app.listen(port, () => console.log(`Node JS Server started at port ${port}!`))
